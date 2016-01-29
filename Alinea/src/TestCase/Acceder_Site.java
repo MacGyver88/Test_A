87216e34a6fe8cfc0b5dd5befe5baccf3eda9c1b
hargui <hargui@ubuntu>
2016-01-29 23:28:56 +0100

    Test case acceder au site

--- Alinea/src/TestCase/Acceder_Site.java
+++ Alinea/src/TestCase/Acceder_Site.java
@@ -1,9 +1,21 @@
 package TestCase;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Acceder_Site {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+        WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://www.alinea.fr");
+		Assert.assertTrue(driver.getPageSource().contains("Alinéa : Mobilier et Décoration."));
+		
+		
+		Thread.sleep(5);
+		driver.close();
 
 	}
 
