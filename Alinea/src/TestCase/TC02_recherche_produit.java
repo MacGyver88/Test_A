3a25633f9b907e2d18bbf9af2bf46e3300891c0a
asmari <asmari@LFR007460.corp.capgemini.com>
2016-01-30 23:54:55 +0100

    TC02

--- Alinea/src/TestCase/TC02_recherche_produit.java
+++ Alinea/src/TestCase/TC02_recherche_produit.java
@@ -1,17 +1,23 @@
 package TestCase;
 
-import java.util.regex.Pattern;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.util.concurrent.TimeUnit;
 
-import org.junit.*;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
-import org.openqa.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.internal.Coordinates;
+import org.openqa.selenium.internal.Locatable;
 
 public class TC02_recherche_produit {
 	
@@ -30,9 +36,49 @@ public class TC02_recherche_produit {
 				.linkText("Canapés, Fauteuils"));
 		Actions action = new Actions(driver);
 		action.moveToElement(Element_move_to_three).build().perform();
+		
+		//résultat attendu step1
+		assertTrue(isElementPresent(By.cssSelector("li.second-level-item.hover")));
+
 
 		driver.findElement(By.cssSelector("a > p")).click();
-	}
+		
+		//résultat attendu step2
+		assertEquals("Canapés, Banquettes", driver.findElement(By.cssSelector("h1")).getText());
+		
+		
+		//Step3 scroll
+		
+		//scroll direct auto vers la fin du page
+
+		//scroll vers le bas en plusieus step
+		
+	
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		for (int second = 0;; second++) {
+
+			    jse.executeScript("window.scrollBy(0,800)", ""); //y value '800' can be altered
+			    Thread.sleep(1000);
+
+		    if(second >=10){
+		        break;
+		    }
+		}
+		
+/*		WebElement element = driver.findElement(By.cssSelector("section.cgv"));
+		Coordinates coordinate = ((Locatable)element).getCoordinates(); 
+		coordinate.onPage(); 
+		coordinate.inViewPort();
+		Thread.sleep(5000);*/
+		
+		//JavascriptExecutor jse = (JavascriptExecutor)driver;
+		//jse.executeScript("window.scrollTo(0,Math.max(document.documentElement.scrollHeight,document.body.scrollHeight,document.documentElement.clientHeight));");
+	
+		
+		
+		}
+		
+
 	
 	@Before
 	public void setUp() throws Exception {
@@ -50,5 +96,14 @@ public class TC02_recherche_produit {
 			fail(verificationErrorString);
 		}
 	}
+	
+	private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
 
 }
