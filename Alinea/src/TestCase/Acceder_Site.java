6a540e9944da98320274436e1628c1858caf48f7
asmari <asmari@LFR007460.corp.capgemini.com>
2016-01-30 12:33:01 +0100

    assert Par produit

--- Alinea/src/TestCase/Acceder_Site.java
+++ Alinea/src/TestCase/Acceder_Site.java
@@ -1,19 +1,23 @@
 package TestCase;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Acceder_Site {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
+
+		
         WebDriver driver = new FirefoxDriver();
 		
 		driver.get("http://www.alinea.fr");
 		Assert.assertTrue(driver.getPageSource().contains("Alinéa : Mobilier et Décoration."));
 		
-		
+		assertEquals("Par produits", driver.findElement(By.cssSelector("li.first-level-item.active")).getText());
 		Thread.sleep(5);
 		driver.close();
 
