c72e381968e046d869f023ec60f84fb884443b62
asmari <asmari@LFR007460.corp.capgemini.com>
2016-01-30 22:30:40 +0100

    TC01 +TC02

--- Alinea/src/TestCase/TC01_Acceder_Site.java
+++ Alinea/src/TestCase/TC01_Acceder_Site.java
@@ -1,29 +1,55 @@
 package TestCase;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TC01_Acceder_Site {
+
+	WebDriver driver = new FirefoxDriver();
+	private StringBuffer verificationErrors = new StringBuffer();
 	
 	
 	@Test
-	public static void main(String[] args) throws InterruptedException {
+	public void TC01() throws Exception {
 
-		
-        WebDriver driver = new FirefoxDriver();
-		
 		driver.get("http://www.alinea.fr");
-		Assert.assertTrue(driver.getPageSource().contains("Alinéa : Mobilier et Décoration."));
-		
-		assertEquals("Par produits", driver.findElement(By.cssSelector("li.first-level-item.active")).getText());
+		Assert.assertTrue(driver.getPageSource().contains(
+				"Alinéa : Mobilier et Décoration."));
+
+		assertEquals(
+				"Par produits",
+				driver.findElement(By.cssSelector("li.first-level-item.active"))
+						.getText());
 		Thread.sleep(5);
 		driver.close();
 
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
 	}
 
 }
--- Alinea/src/TestCase/TC02_recherche_produit.java
+++ Alinea/src/TestCase/TC02_recherche_produit.java
@@ -1,27 +1,54 @@
 package TestCase;
 
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 public class TC02_recherche_produit {
 	
 	
-	@Test
-	public static void main(String[] args) throws InterruptedException {
-	
 	WebDriver driver = new FirefoxDriver();
-	driver.get("http://www.alinea.fr");
-	
-	WebElement Element_move_to_three = driver.findElement(By.linkText( "Canapés, Fauteuils"));
-	Actions action = new Actions(driver);
-	action.moveToElement(Element_move_to_three).build().perform();
+	private StringBuffer verificationErrors = new StringBuffer();
 	
 	
-	
-	driver.findElement(By.cssSelector("a > p")).click();
+	@Test
+	public void TC02() throws Exception {
+
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.alinea.fr");
+
+		WebElement Element_move_to_three = driver.findElement(By
+				.linkText("Canapés, Fauteuils"));
+		Actions action = new Actions(driver);
+		action.moveToElement(Element_move_to_three).build().perform();
+
+		driver.findElement(By.cssSelector("a > p")).click();
 	}
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
 }
